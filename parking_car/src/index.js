2ec3bd34b023923d218d598a7fefcd3cc9dc3e42
Emerson Silva <emerson_jdss@hotmail.com>
2019-08-19 23:51:29 -0300

    insert fixed bar bottom

--- parking_car/src/index.js
+++ parking_car/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, View, Text} from 'react-native';
 import MapView from 'react-native-maps';
 const App = () => {
   return (
@@ -14,8 +14,12 @@ const App = () => {
         style={{
           height: '100%',
           width: '100%',
+          position: 'relative',
         }}
       />
+      <View style={styles.containerMain}>
+        <Text>Teste</Text>
+      </View>
     </View>
   );
 };
@@ -23,8 +27,17 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    position: 'relative',
+    backgroundColor: '#cecece',
+    zIndex: 0,
+  },
+  containerMain: {
+    bottom: 0,
+    zIndex: 1,
+    height: 100,
+    width: '100%',
+    backgroundColor: 'orange',
+    position: 'absolute',
   },
 });
 export default App;
