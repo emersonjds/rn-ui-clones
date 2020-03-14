fc80ac8b5cce898be8cc03e0969f5a00a01f8e68
Emerson Silva <emerson_jdss@hotmail.com>
2020-03-14 19:35:13 -0300

    styled ContainersTab

--- nubank/src/components/Tabs/index.js
+++ nubank/src/components/Tabs/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
-import {Container} from './styles';
+import {Container, TabsContainer, TabItem, TabText} from './styles';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+Icon.loadFont();
 
 export default function Tabs() {
-  return <Container />;
+  return (
+    <Container>
+      <TabsContainer>
+        <TabItem>
+          <Icon name="person-add" size={24} color="#FFF" />
+          <TabText>Indicar amigos</TabText>
+        </TabItem>
+      </TabsContainer>
+    </Container>
+  );
 }
