4bde0b5f837f91236e675ba1b24f4b876b47c902
Emerson Silva <emerson_jdss@hotmail.com>
2020-03-14 17:11:06 -0300

    stater nubank ui

---
+++ nubank/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text, SafeAreaView} from 'react-native';
+
+// import { Container } from './styles';
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Emerson</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
