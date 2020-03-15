97daeedb205c5edfc3de53e0bcd65463e53a8b83
Emerson Silva <emerson_jdss@hotmail.com>
2020-03-15 14:16:21 -0300

    config animations with interpolate

--- nubank/src/Main/index.js
+++ nubank/src/Main/index.js
@@ -5,6 +5,8 @@ Icon.loadFont();
 import Header from '../components/Header';
 import Tabs from '../components/Tabs';
 import Menu from '../components/Menu';
+import {Animated} from 'react-native';
+import {PanGestureHandler, State} from 'react-native-gesture-handler';
 
 import {
   Container,
@@ -12,7 +14,6 @@ import {
   Card,
   CardHeader,
   CardContent,
-  Cardfooter,
   Description,
   Title,
   Annotation,
@@ -20,28 +21,84 @@ import {
 } from './styles';
 
 export default function Main() {
+  let offset = 0;
+
+  const translateY = new Animated.Value(0);
+  const animatedEvent = Animated.event(
+    [
+      {
+        nativeEvent: {
+          translationY: translateY,
+        },
+      },
+    ],
+    {
+      useNativeDriver: true,
+    },
+  );
+
+  function onHandlerStateChanged(event) {
+    if (event.nativeEvent.oldState === State.ACTIVE) {
+      let opened = false;
+      const {translationY} = event.nativeEvent;
+      offset += translationY;
+
+      if (translationY >= 100) {
+        opened = true;
+      } else {
+        translateY.setValue(offset);
+        translateY.setOffset(0);
+        offset = 0;
+      }
+      Animated.timing(translateY, {
+        toValue: opened ? 380 : 0,
+        duration: 200,
+        useNativeDriver: true,
+      }).start(() => {
+        offset = opened ? 380 : 0;
+        translateY.setOffset(offset);
+        translateY.setValue(0);
+      });
+    }
+  }
+
   return (
     <Container>
       <Header />
       <Content>
-        <Menu />
-        <Card>
-          <CardHeader>
-            <Icon name="attach-money" size={28} color="#666" />
-            <Icon name="visibility-off" size={28} color="#666" />
-          </CardHeader>
-          <CardContent>
-            <Title>Saldo disponivel</Title>
-            <Description>R$ 589.611,65</Description>
-          </CardContent>
-          <CardFooter>
-            <Annotation>
-              Trasnferencia recebida de Rafael no valor de R$ 20,00
-            </Annotation>
-          </CardFooter>
-        </Card>
+        <Menu translateY={translateY} />
+        <PanGestureHandler
+          onGestureEvent={animatedEvent}
+          onHandlerStateChange={onHandlerStateChanged}>
+          <Card
+            style={{
+              transform: [
+                {
+                  translateY: translateY.interpolate({
+                    inputRange: [-350, 0, 380],
+                    outputRange: [-50, 0, 380],
+                    extrapolate: 'clamp',
+                  }),
+                },
+              ],
+            }}>
+            <CardHeader>
+              <Icon name="attach-money" size={28} color="#666" />
+              <Icon name="visibility-off" size={28} color="#666" />
+            </CardHeader>
+            <CardContent>
+              <Title>Saldo disponivel</Title>
+              <Description>R$ 589.611,65</Description>
+            </CardContent>
+            <CardFooter>
+              <Annotation>
+                Trasnferencia recebida de Rafael no valor de R$ 20,00
+              </Annotation>
+            </CardFooter>
+          </Card>
+        </PanGestureHandler>
       </Content>
-      <Tabs />
+      <Tabs translateY={translateY} />
     </Container>
   );
 }
--- nubank/src/components/Menu/index.js
+++ nubank/src/components/Menu/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import {View, Image} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+Icon.loadFont();
 
-import {Container, Code} from './styles';
+import {
+  Container,
+  Code,
+  Nav,
+  NavItem,
+  NavText,
+  SignOutButton,
+  SignOutButtonText,
+} from './styles';
 
-export default function Menu() {
+export default function Menu({translateY}) {
   return (
-    <Container>
+    <Container
+      style={{
+        opacity: translateY.interpolate({
+          inputRange: [0, 150],
+          outputRange: [0, 1],
+        }),
+      }}>
       <Code>
         <Image
           style={{width: 80, height: 80}}
@@ -15,6 +31,27 @@ export default function Menu() {
           }}
         />
       </Code>
+      <Nav>
+        <NavItem>
+          <Icon name="help-outline" size={20} color="#FFF" />
+          <NavText>Me ajude</NavText>
+        </NavItem>
+        <NavItem>
+          <Icon name="person-outline" size={20} color="#FFF" />
+          <NavText>Perfil</NavText>
+        </NavItem>
+        <NavItem>
+          <Icon name="credit-card" size={20} color="#FFF" />
+          <NavText>Configurar Cartao</NavText>
+        </NavItem>
+        <NavItem>
+          <Icon name="smartphone" size={20} color="#FFF" />
+          <NavText>Configuracoes do App</NavText>
+        </NavItem>
+      </Nav>
+      <SignOutButton onPress={() => {}}>
+        <SignOutButtonText>Sair do App</SignOutButtonText>
+      </SignOutButton>
     </Container>
   );
 }
