4b2985ab446cdb2a3a1167751a4f9eec96e54863
Emerson Silva <emerson_jdss@hotmail.com>
2020-03-14 23:38:05 -0300

    start main card content

--- nubank/src/Main/index.js
+++ nubank/src/Main/index.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons'/
+Icon.loadFont();
+
 import Header from '../components/Header';
 import Tabs from '../components/Tabs';
 
@@ -8,6 +11,27 @@ export default function Main() {
   return (
     <Container>
       <Header />
+      <Content>
+        <Card>
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="#666" />
+            <Icon name="visibility-off" size={28} color="#666" />
+          </CardHeader>
+          <CardContent>
+            <Title>
+              Saldo disponivel
+            </Title>
+            <Description>
+              R$ 589.611,65
+            </Description>
+          </CardContent>
+          <Cardfooter>
+            <Anntation>
+              Trasnferencia recebida
+            </Anntation>
+          </Cardfooter>
+        </Card>
+      </Content>
       <Tabs />
     </Container>
   );
